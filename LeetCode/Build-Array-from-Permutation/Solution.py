180d6ad244a38dcb41855cd91635547a917e364f
RATHISRI A <174573004+rathisri-a@users.noreply.github.com>
2026-08-08 16:38:00 +0530

    Add solution for Build Array from Permutation

--- LeetCode/Build-Array-from-Permutation/Solution.py
+++ LeetCode/Build-Array-from-Permutation/Solution.py
@@ -1,2 +1,9 @@
-def buildArray(self, nums: List[int]) -> List[int]:
-        return [nums[i] for i in nums]
+1class Solution(object):
+2    def buildArray(self, nums):
+3        ans=[]
+4        n=len(nums)
+5        for i in range(n):
+6            ans.append(nums[nums[i]])
+7        return ans
+8
+9        
