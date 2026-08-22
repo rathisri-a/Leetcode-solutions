110b72d480d56d832b08cbfcdd63d34742734426
RATHISRI A <174573004+rathisri-a@users.noreply.github.com>
2026-08-22 21:37:36 +0530

    Add solution for Contains Duplicate

--- LeetCode/Contains-Duplicate/Solution.py
+++
@@ -1,9 +0,0 @@
-1class Solution(object):
-2    def containsDuplicate(self, nums):
-3        n=set()
-4        for i in nums:
-5            if i in n:
-6                return True
-7            n.add(i)
-8        return False
-9        
